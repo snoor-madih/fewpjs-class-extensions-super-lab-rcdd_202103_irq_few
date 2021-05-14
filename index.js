0adb5ae7fc276f9281c151c366744ac643a7b1c9
snoor_madih <snoor.madih@gmail.com>
2021-05-14 15:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// Write your classes here
-class Tree
-{
-  constractor(species)
-  {
-    this.species=species;
-  }
-  static definition()
-  {
-    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
-  }
-}
+// // Write your classes here
+// class Tree
+// {
+//   constractor(species)
+//   {
+//     this.species=species;
+//   }
+//   static definition()
+//   {
+//     return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+//   }
+// }
 
-class Deciduous extends Tree
-{
-  constractor(species,name)
-  {
-    super(species);
-    this.name=name;
-  }
-  static definition() {
-    return `${super.definition()} Deciduous trees shed their leaves annually.`
-  }
-}
+// class Deciduous extends Tree
+// {
+//   constractor(species,name)
+//   {
+//     super(species);
+//     this.name=name;
+//   }
+//   static definition() {
+//     return `${super.definition()} Deciduous trees shed their leaves annually.`
+//   }
+// }
 
-class Evergreen extends trees{
-  constractor(species,name)
-  {
-    super(species);
-    this.name=name;
-  }
-  static definition()
-  {
-   return `${super.definition()} Deciduous trees shed their leaves annually.`
-  }
-}
+// class Evergreen extends trees{
+//   constractor(species,name)
+//   {
+//     super(species);
+//     this.name=name;
+//   }
+//   static definition()
+//   {
+//   return `${super.definition()} Deciduous trees shed their leaves annually.`
+//   }
+// }
